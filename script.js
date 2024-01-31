c251730f20b1f04d56c2d98a0c8837ab201d7e86
supersuper261 <148677532+supersuper261@users.noreply.github.com>
2024-01-31 21:08:48 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-function toggleEnvelope(envelope) {
-    envelope.classList.toggle("open");
+function toggleEnvelope() {
+    const envelope = document.querySelector('.envelope');
+    envelope.classList.toggle('open');
 }
 
+function sendLetter() {
+    const letter = document.querySelector('.letter');
+    letter.style.transition = 'transform 1s ease';
+    letter.style.transform = 'translateX(100%)';
+}
